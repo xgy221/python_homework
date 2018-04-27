830e688965e4d9a7a1a0e2862cf169876e532def
xgy221 <2406753528@qq.com>
2018-04-27 18:22:47 +0800

    subject

---
+++ homework_4_27.py
@@ -0,0 +1,20 @@
+"""有些REST API 会把参数放到URL中，比如Github的API：
+GET/users/:user/repos,调用时，需要把：user替换为实际
+用户名。如果我们能写出这样的链式调用：
+               Chain.users('michael').repos"""
+
+
+class Chain(object):
+
+    def __init__(self, path=''):
+        print(path)
+        self._path = path
+
+    def __getattr__(self, path):
+        return Chain('%s/%s' % (self._path, path))
+
+    def __str__(self):
+        return self._path
+
+
+print(Chain().users('michael').repos)
